fd9596ad5aefb3dcac7d71f77bcdb7436b951fe5
ajay <ajay.kumar@credencys.com>
2017-04-10 19:08:51 +0530

    aprl10

--- app/src/main/java/upgradekaro/a3dmovies/MoviesModel.java
+++ app/src/main/java/upgradekaro/a3dmovies/MoviesModel.java
@@ -5,41 +5,13 @@ package upgradekaro.a3dmovies;
  */
 
 public class MoviesModel {
+
+
     String id;
+    String url;
+    String imdb_code;
     String title;
+    String title_english;
     String year;
-
-    public MoviesModel() {
-    }
-
-    public MoviesModel(String id, String title, String year) {
-
-        this.id = id;
-        this.title = title;
-        this.year = year;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getYear() {
-        return year;
-    }
-
-    public void setYear(String year) {
-        this.year = year;
-    }
+    String rating;
 }
